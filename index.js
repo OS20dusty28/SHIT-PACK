531cc2c29c4a99233d5cf7244e7a30daf70151e1
OS20dusty28 <63958372+OS20dusty28@users.noreply.github.com>
2021-01-26 11:04:20 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Project yeah</title>
+	<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="container">
+	    <div class="menu">		
+			    <img src="main.png" class="logo">
+				<ul>
+				</ul>
+		</div>
+	</div>
+	
+</body>
+</html>
